9574c76b1d4000ef32f33a7de70b1aab27182298
Radeon590 <59920979+Radeon590@users.noreply.github.com>
2021-10-25 23:47:55 +0300

    очистка массива и всякая хуйня. спросить про элемент вызывающий event

--- js/game.js
+++ js/game.js
@@ -9,6 +9,7 @@ function main(){
 
     let container = document.getElementById('container');
 
+    //создание массива для циферок
     let numbers = [];
 
     for(let i = 0; i < columns; i++) {
@@ -17,30 +18,40 @@ function main(){
     }
 
     for(let i = 0; i < columns; i++) {
+        //создаем колонку
         let newColumn = document.createElement('div');
         newColumn.className = 'cellsColumn';
+        //создаем клетки в колонку
         for(let j = 0; j < columns; j++) {
             let cell = document.createElement('div');
             cell.className = 'cell';
+            cell.addEventListener('click', onCellClick);
+            newColumn.appendChild('cell');
+            //определем циферку("картинку") для клетки
             let numbersIndex = getRandomInt(numbers.length - 1);
             let cellIndex = numbers[numbersIndex];
             numbers[numbersIndex] = null;
-            numbers = numbers.map(getRandomInt);
+            numbers = getClearArray(numbers);
             cell.id = cellIndex;
-            newColumn.appendChild('cell');
         }
         container.appendChild(newColumn);
     }
 }
 
 function onCellClick(){
-
-}
-
-function CheckArrayIndexes(){
-
+    
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
+}
+
+function getClearArray(arr){
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] != null){
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
