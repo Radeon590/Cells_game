bac3ca23482284452c1a6aa7ab9a0b5f5b835f75
Radeon590 <59920979+Radeon590@users.noreply.github.com>
2021-10-29 12:33:59 +0300

    Готово но пока не работает

--- js/game.js
+++ js/game.js
@@ -1,9 +1,21 @@
 //const { getCookie } = require("../cookieLib/cookie");
 
+let dnkSwitched = false;
+let lastCell = null;
+let savedCell = null;
+let idToReset = null;
+
 window.onload = main;
 
-function main(){
-    let cellsNumber = getCookie('cellsNumber');
+function main() {
+    //easter egg moment
+    let scoreEasterEgg = document.getElementById('Score');
+    scoreEasterEgg.onclick = switchToDNK;
+    //
+    initializeCells();
+
+
+    /*let cellsNumber = getCookie('cellsNumber');
 
     let columns = cellsNumber / 2;
 
@@ -35,23 +47,109 @@ function main(){
             cell.id = cellIndex;
         }
         container.appendChild(newColumn);
+    }*/
+}
+
+function switchToDNK() {
+    if (!dnkSwitched) {
+        let cells = document.getElementsByClassName('Cells');
+
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].classList.add('DNK');
+        }
+
+        let background = document.getElementById('background');
+        background.classList.add('DNK');
+        dnkSwitched = true;
+    }
+    else {
+        let cells = document.getElementsByClassName('Cells');
+
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].classList.remove('DNK');
+        }
+
+        let background = document.getElementById('background');
+        background.classList.remove('DNK');
+        dnkSwitched = false;
     }
 }
 
-function onCellClick(){
-    
+function initializeCells() {
+    let cells = document.getElementsByClassName('Cells');
+
+    let numbers = [];
+
+    for (let i = 0; i < 8; i++) {
+        numbers.push(i + 1);
+    }
+    for (let i = 0; i < 8; i++) {
+        numbers.push(i + 1);
+    }
+
+    for (let i = 0; i < cells.length; i++) {
+        let index = getRandomInt(numbers.length - 1);
+        cells[i].id = "Cell" + numbers[index];
+        cells[i].onclick = onCellClick;
+        numbers[index] = null;
+        numbers = getClearArray(numbers);
+    }
+}
+
+function onCellClick() {
+    console.log('click');
+    if (lastCell != null) {
+        if (lastCell.id == this.id) {
+            setTimeout(disableCell, 1000);
+        }
+        else {
+            savedCell = this;
+            setTimeout(closeWrongCells, 1000);
+        }
+    }
+    else {
+        lastCell = this;
+    }
+}
+
+function closeWrongCells() {
+    if (savedCell != null) {
+        lastCell.click();
+        savedCell.removeAttribute('onclick');
+        savedCell.click();
+        savedCell.onclick = onCellClick;
+        savedCell = null;
+        lastCell = null;
+    }
+
+}
+
+function disableCell() {
+    this.setAttribute('disabled', true);
+    lastCell = null;
+}
+
+function resetID() {
+    lastCell = idToReset;
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function getClearArray(arr){
+function getClearArray(arr) {
     let newArr = [];
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] != null){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] != null) {
             newArr.push(arr[i]);
         }
     }
     return newArr;
+}
+
+function playMusic() {
+    console.log("play");
+    var audio = new Audio('source/chill.wav');
+    audio.play(); // Автоматически запускаем
+    console.log("played");
 }
